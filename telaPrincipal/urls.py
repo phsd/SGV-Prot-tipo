9e65b5ad1c03fab347b2636665e8052e7dc7afbc
Paulo <paulohenrique@live.com.ph>
2019-03-22 22:58:39 -0300

    Sistema de Login adicionado

--- telaPrincipal/urls.py
+++ telaPrincipal/urls.py
@@ -7,12 +7,12 @@ from django.views.generic.base import TemplateView
 app_name = 'telaPrincipal'
 
 urlpatterns = [
-    url('accounts/login/', auth_views.LoginView.as_view(), name='loginForm'),
     url(r'^$', view=views.index, name='index'),
     url(r'^maquina/(?P<id_maquina>[-\w]+)/(?P<id_estrutura>[-\w]+)/$', view=views.maquina, name='urlMaquina'),
     url(r'^formMaquina', views.formularioMaquina, name='urlFormMaquina'),
     url(r'^FormEstrutura', views.formularioEstrutura, name='urlFormEstrutura'),
     url(r'^ajax/carregarEstruturas/', views.carregarEstruturas, name='ajaxCarregarEstruturas'),
     url(r'^ajax/carregarPrazosEstrutura/', views.carregarPrazosEstrutura, name='ajaxCarregarPrazosEstrutura'),
+    url('accounts/login/', auth_views.LoginView.as_view(template_name='login.html'), name='loginForm'),
     url('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
