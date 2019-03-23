b7bf02fc68b2e33bb49e5acf6ac512d02e0f4fd3
Paulo <paulohenrique@live.com.ph>
2019-03-22 23:11:30 -0300

    Sistema de Login adicionado

--- sgv/settings.py
+++ sgv/settings.py
@@ -149,7 +149,7 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/home/sgvl/SGV-Prot-tipo/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 LOGIN_REDIRECT_URL = '/'
