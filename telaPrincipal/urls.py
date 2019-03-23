ae6ab3796b836cc6c7688ea7e900329a97bdf181
Paulo <paulohenrique@live.com.ph>
2019-03-22 22:28:53 -0300

    Sistema de Login adicionado

--- telaPrincipal/urls.py
+++ telaPrincipal/urls.py
@@ -13,6 +13,6 @@ urlpatterns = [
     url(r'^FormEstrutura', views.formularioEstrutura, name='urlFormEstrutura'),
     url(r'^ajax/carregarEstruturas/', views.carregarEstruturas, name='ajaxCarregarEstruturas'),
     url(r'^ajax/carregarPrazosEstrutura/', views.carregarPrazosEstrutura, name='ajaxCarregarPrazosEstrutura'),
-    url(r'^accounts/login/', auth_views.LoginView.as_view(), name="login"),
+    url(r'^accounts/login/', auth_views.LoginView.as_view(), name="loginForm"),
     url(r'^accounts/logout/', auth_views.LogoutView.as_view(), name="logout"),
 ]
