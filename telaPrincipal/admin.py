52c38964c53a178442c0acc825397593c7e9d114
Paulo <paulohenrique@live.com.ph>
2019-03-23 15:34:27 -0300

    Sistema de Login adicionado

--- telaPrincipal/admin.py
+++ telaPrincipal/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Maquinas
+from .models import Estruturas
 from .models import Estrutura
 from .models import Maquina
 
