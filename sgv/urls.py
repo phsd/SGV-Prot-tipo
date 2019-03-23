c47fda6ccf0c2187a1c4abbb8d672b7289698996
Paulo <paulohenrique@live.com.ph>
2019-03-23 19:09:11 -0300

    Sistema de Login adicionado

--- sgv/urls.py
+++ sgv/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url, include
 
+admin.autodiscover()
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('telaPrincipal.urls', namespace='telaPrincipal')),
