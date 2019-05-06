02eefb3440d28f4c24b93621a8995eb59b7a56f3
Paulo <paulohenrique@live.com.ph>
2019-05-06 20:39:39 -0300

    Hourly Schedule Management

--- telaPrincipal/admin.py
+++ telaPrincipal/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Maquinas, Maquina, Estruturas, Estrutura
+from .models import Maquinas, Maquina, Estruturas, Estrutura, Setores, Locais, HourlyScheduleManagement, HourlyScheduleManagementRealizado
 
 admin.site.register(Maquinas)
 admin.site.register(Maquina)
 admin.site.register(Estruturas)
 admin.site.register(Estrutura)
+admin.site.register(Setores)
+admin.site.register(Locais)
+admin.site.register(HourlyScheduleManagement)
+admin.site.register(HourlyScheduleManagementRealizado)
